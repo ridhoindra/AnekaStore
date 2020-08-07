b165251d54081d88462d5728acaa0fa6a67fd3e4
indrachelsea <ridho_indra_27rpl@student.smktelkom-mlg.sch.id>
2020-08-07 18:59:37 +0700

    swagger documentation

--- app.js
+++ app.js
@@ -5,6 +5,27 @@ const mysql = require('mysql')
 const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
 
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      version: "1.0.0",
+      title: "Customer API",
+      description: "AnekaStore",
+      contact: {
+        name: "Ridho Indra"
+      },
+      servers: ["http://localhost:3000"]
+    }
+  },
+  // ['.routes/*.js']
+  apis: ["app.js"]
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
@@ -21,6 +42,18 @@ const db = mysql.createConnection({
     console.log("Database connected");
   });
 
+
+  /**
+ * @swagger
+ * /user:
+ *  get:
+ *    tags:
+ *        - Get User
+ *    description: Use to request all customers
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
   app.get("/user",(req,res)=>{
     db.query(`select * from user`,
     (err, result) => {
@@ -31,7 +64,37 @@ const db = mysql.createConnection({
       });
     }
   );
-  })
+})
+
+/**
+ * @swagger
+ * /addUser:
+ *  post:
+ *    tags:
+ *        - CRUD User
+ *    description: Use to create a user data
+ *    parameters:
+ *        - name: reqBody
+ *          description: request body
+ *          in: body
+ *          schema:
+ *            type: object
+ *            properties:
+ *              username:
+ *                type: string
+ *              email:
+ *                type: string
+ *                format: email
+ *              password: 
+ *                type: string
+ *            required:
+ *                - username
+ *                - email
+ *                - password
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
   app.post("/addUser", (req, res) => {
       const username= req.body.username;
       const email= req.body.email;
@@ -53,8 +116,49 @@ const db = mysql.createConnection({
               });
         }
     })
+
+    // Coba Swagger
+    // res.status(200).json({
+    //   username,email,password
+    // })
   });
 
+  /**
+ * @swagger
+ * /editUser/{id}:
+ *  put:
+ *    tags:
+ *        - CRUD User
+ *    description: Use to edit a user data by id
+ *    parameters:
+ *        - name : id
+ *          description: id to update
+ *          in: path
+ *          type: integer
+ *          required: true
+ *        - name: reqBody
+ *          description: request body
+ *          in: body
+ *          schema:
+ *            type: object
+ *            properties:
+ *              username:
+ *                type: string
+ *              alamat:
+ *                type: string
+ *              telepon: 
+ *                type: string
+ *              status:
+ *                type: string
+ *            required:
+ *                - username
+ *                - alamat
+ *                - telepon
+ *                - status
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
   app.put("/editUser/:id", function(req, res) {
     let data = 
       'UPDATE user SET username="' +
@@ -78,6 +182,23 @@ const db = mysql.createConnection({
     });
   });
   
+  /**
+ * @swagger
+ * /deleteUser/{id}:
+ *  delete:
+ *    tags:
+ *        - CRUD User
+ *    description: Use to delete a user data by id
+ *    parameters:
+ *        - name : id
+ *          description: id to update
+ *          in: path
+ *          type: integer
+ *          required: true
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
   app.delete("/deleteUser/:id", function(req, res) {
     let id = "delete from user where id=" + req.params.id;
   
@@ -92,6 +213,33 @@ const db = mysql.createConnection({
     });
   });
 
+  /**
+ * @swagger
+ * /softdelete/{id}:
+ *  put:
+ *    tags:
+ *        - CRUD User
+ *    description: Use to edit a status user data by id
+ *    parameters:
+ *        - name : id
+ *          description: id to update
+ *          in: path
+ *          type: integer
+ *          required: true
+ *        - name: reqBody
+ *          description: request body
+ *          in: body
+ *          schema:
+ *            type: object
+ *            properties:
+ *              status:
+ *                type: string
+ *            required:
+ *                - status
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
   app.put("/softdelete/:id", function(req, res) {
     let data = 
       'UPDATE user SET status="' +
@@ -110,7 +258,32 @@ const db = mysql.createConnection({
   });
 
 
-
+/**
+ * @swagger
+ * /api/login:
+ *  post:
+ *    tags:
+ *        - API Login User
+ *    description: Use to login as user
+ *    parameters:
+ *        - name: reqBody
+ *          description: request body
+ *          in: body
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *                format: email
+ *              password: 
+ *                type: string
+ *            required:
+ *                - email
+ *                - password
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 app.post("/api/login", (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
@@ -144,6 +317,42 @@ app.post("/api/login", (req, res) => {
     }
   });
 
+  /**
+ * @swagger
+ * /api/editUser/{id}:
+ *  put:
+ *    tags:
+ *        - API Edit User
+ *    description: Use to edit a user data by id
+ *    parameters:
+ *        - name : id
+ *          description: id to update
+ *          in: path
+ *          type: integer
+ *          required: true
+ *        - name: reqBody
+ *          description: request body
+ *          in: body
+ *          schema:
+ *            type: object
+ *            properties:
+ *              username:
+ *                type: string
+ *              alamat:
+ *                type: string
+ *              telepon: 
+ *                type: string
+ *              password:
+ *                type: string
+ *            required:
+ *                - username
+ *                - alamat
+ *                - telepon
+ *                - password
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
   app.put("/api/editUser/:id", function(req, res) {
     let data = 
       'UPDATE user SET username="' +
